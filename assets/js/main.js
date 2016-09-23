e638753688b7f74da159ae434de076517d7ee80c
Jonathan Grider <jagrider@purdue.edu>
2016-09-22 22:32:05 -0400

    tyring to add main.js for scrolling

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+/*
+* Stuff that needs done when the page loads.
+*/
+function () {
+  registerNavButtons();
+}
+
+/*
+* registerNavButtons lets us register the buttons for about and contact
+*/
+function registerNavButtons() {
+  var sections = ["about", "contact"];
+
+  sections.forEach(function(section) {
+    clickAndScroll("#nav-" + section, "#" + section + "-section");
+  });
+}
+
+/*
+* Smoothens scrolling when About or Contact are selected from the navbar 
+*/
+function clickAndScroll(link, section) {
+  var delay = 700;
+
+  $(link).click(function() {
+    $("html, body").animate({
+      scrollTop: $(section).offset().top
+    }, delay);
+
+    return false;
+  });
+}
