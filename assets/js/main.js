e7988d73c33c3d7f0c87deeb18c2bfbc7af0c663
Jonathan Grider <jagrider@purdue.edu>
2018-08-30 01:58:59 -0400

    Changed null to undefined - le sign again

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,7 @@
 function scroll_to(clicked_link, nav_height) {
 	var element_class = clicked_link.attr('href').replace('#', '.');
 	var scroll_to = 0;
-	if(element_class !== null) {
+	if(clicked_link !== undefined) {
 		element_class += '-container';
 		scroll_to = $(element_class).offset().top - nav_height;
 	}
