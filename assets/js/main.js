49b4073f85484f01c923de842159693ef1272d43
Jonathan Grider <jagrider@purdue.edu>
2018-08-30 00:53:54 -0400

    Changing scroll functionality in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,51 @@
-/*
-* Stuff that needs done when the page loads.
-*/
-$(function() {
-  registerNavButtons();
-});
+// /*
+// * Stuff that needs done when the page loads.
+// */
+// $(function() {
+//   registerNavButtons();
+// });
+//
+// /*
+// * registerNavButtons lets us register the buttons for about and contact
+// */
+// function registerNavButtons() {
+//   var sections = ["about", "contact", "experience", "projects", "skills"];
+//
+//   sections.forEach(function(section) {
+//     clickAndScroll("#nav-" + section, "#" + section);
+//   });
+// }
+//
+// /*
+// * Smoothens scrolling when About or Contact are selected from the navbar
+// */
+// function clickAndScroll(link, section) {
+//   var delay = 700;
+//
+//   $(link).click(function() {
+//     $("html, body").animate({
+//       scrollTop: $(section).offset().top
+//     }, delay);
+//
+//     return false;
+//   });
+// }
 
-/*
-* registerNavButtons lets us register the buttons for about and contact
-*/
-function registerNavButtons() {
-  var sections = ["about", "contact", "experience", "projects", "skills"];
+jQuery(document).ready(function () {
 
-  sections.forEach(function(section) {
-    clickAndScroll("#nav-" + section, "#" + section);
-  });
-}
+    /*
+      Used for Navigation
+    */
+     $('a[href^="#"]').on('click', function (e) {
+         e.preventDefault();
 
-/*
-* Smoothens scrolling when About or Contact are selected from the navbar
-*/
-function clickAndScroll(link, section) {
-  var delay = 700;
+         var target = this.hash,
+             $target = $(target);
 
-  $(link).click(function() {
-    $("html, body").animate({
-      scrollTop: $(section).offset().top
-    }, delay);
-
-    return false;
-  });
-}
+         $('html, body').stop().animate({
+             'scrollTop': $target.offset().top - 80
+         }, 900, 'swing', function () {
+             window.location.hash = target;
+         });
+     });
+ });
