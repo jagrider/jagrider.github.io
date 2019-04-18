2a66684c4a53e7926bc70d62d2e95c5d459384cd
Jonathan Grider <jgrider2015@gmail.com>
2019-04-17 23:22:19 -0400

    Added Spring 2019 overhaul

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,64 @@
-// /*
-// * Stuff that needs done when the page loads.
-// */
-// $(function() {
-//   registerNavButtons();
-// });
-//
-// /*
-// * registerNavButtons lets us register the buttons for about and contact
-// */
-// function registerNavButtons() {
-//   var sections = ["about", "contact", "experience", "projects", "skills"];
-//
-//   sections.forEach(function(section) {
-//     clickAndScroll("#nav-" + section, "#" + section);
-//   });
-// }
-//
-// /*
-// * Smoothens scrolling when About or Contact are selected from the navbar
-// */
-// function clickAndScroll(link, section) {
-//   var delay = 700;
-//
-//   $(link).click(function() {
-//     $("html, body").animate({
-//       scrollTop: $(section).offset().top
-//     }, delay);
-//
-//     return false;
-//   });
-// }
-//
+/*
+* Stuff that needs done when the page loads.
+*/
+$(function() {
+  registerNavButtons();
+});
 
-function scroll_to(clicked_link, nav_height) {
-  var element_class = clicked_link.attr('href').replace('#', '.');
-	var scroll_to = 0;
-	element_class += '-container';
-	scroll_to = $(element_class).offset().top - nav_height;
-	if($(window).scrollTop() != scroll_to) {
-		$('html, body').stop().animate({scrollTop: scroll_to}, 600);
-	}
+/*
+* registerNavButtons lets us register the buttons for about and contact
+*/
+function registerNavButtons() {
+  var sections = ["about", "experience", "projects", "skills", "contact"];
+
+  sections.forEach(function(section) {
+    clickAndScroll("#nav-" + section, "#" + section);
+  });
 }
 
-jQuery(document).ready(function () {
+/*
+* Smoothens scrolling when About or Contact are selected from the navbar
+*/
+function clickAndScroll(link, section) {
+  var delay = 700;
 
-    /*
-      Used for Navigation
-    */
-    $('a.scroll-link').on('click', function(e) {
-      e.preventDefault();
-      scroll_to($(this), $('nav').outerHeight());
-    });
+  $(link).click(function() {
+    $("html, body").animate({
+      scrollTop: $(section).offset().top
+    }, delay);
 
-    $('a.top-scroll').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').stop().animate({scrollTop: 0}, 600);
-    });
- });
+    return false;
+  });
+}
+
+
+// function scroll_to(clicked_link, nav_height) {
+//   if (!clicked_link.includes("/")) {
+//     var element_class = clicked_link.attr('href').replace('#', '.');
+//   	var scroll_to = 0;
+//   	element_class += '-section';
+//   	scroll_to = $(element_class).offset().top - nav_height;
+//     if (element_class == 'about-section') {
+//       scroll_to = $(element_class).offset().top;
+//     }
+//   	if($(window).scrollTop() != scroll_to) {
+//   		$('html, body').stop().animate({scrollTop: scroll_to}, 600);
+//   	}
+//   }
+// }
+//
+// jQuery(document).ready(function () {
+//
+//     /*
+//       Used for Navigation
+//     */
+//     $('a.scroll-link').on('click', function(e) {
+//       e.preventDefault();
+//       scroll_to($(this), $('nav').outerHeight());
+//     });
+//
+//     $('a.top-scroll').on('click', function(e) {
+//       e.preventDefault();
+//       $('html, body').stop().animate({scrollTop: 0}, 600);
+//     });
+//  });
